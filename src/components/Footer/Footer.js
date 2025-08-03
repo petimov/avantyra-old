7874a5bdd6d000723e90ee8e345cd363c0b14df6
John Doe <john@example.com>
2025-08-03 02:36:18 +0200

    Fix footer and clean up files

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Footer.css'
 import logoIMG from  '../../images/kavarna_avantyra_logo.webp'
+
 import { Link } from 'react-scroll'
 
 const Footer = () => {
   return (
     <div className='footer'>
         <div>
-            <div className='footer-logo'><Link to='main-view-page' smooth><Logo /></Link></div>
             <div className='footer-logo'><Link to='main-view-page' smooth><img src={logoIMG} /></Link></div>
             <div>
                 <p>Copyright &copy; {(new Date().getFullYear())} kavárna Avantýra</p>
